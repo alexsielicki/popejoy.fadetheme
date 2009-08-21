917d9e9fdbb142d9501639d4997ac6693f2d314a
Alex Sielicki <alex@fostermilo.com>
2009-08-20 18:32:56 -0600

    updating homepage upcoming shows

---
+++ popejoy/fadetheme/skins/popejoy_fadetheme_custom_templates/getHomepageUpcomingShows.py
@@ -0,0 +1,11 @@
+limit = 3
+parentIds = []
+upcomingShows = []
+performances = context.queryCatalog({'portal_type':'Performance', 'sort_on':'start', 'start': {'query': DateTime(), 'range': 'min'}})
+for performance in performances:
+    performanceObject = performance.getObject()
+    performanceParentUID = performanceObject.aq_inner.aq_parent.UID()
+    if performanceParentUID not in parentIds:
+        parentIds.append(performanceParentUID)
+        upcomingShows.append(performanceObject)
+return upcomingShows
